c135bc0b5e7ea0015e85562d8863cc39ef52f404
Isaac Robinson <awesomeisaac2000@gmail.com>
2022-12-06 13:14:35 -0500

    Marker size pulled from sleap preferences.

--- diplomat/frontends/sleap/visual_settings.py
+++ diplomat/frontends/sleap/visual_settings.py
@@ -2,6 +2,7 @@ from diplomat.processing.containers import ConfigSpec
 import diplomat.processing.type_casters as tc
 import matplotlib.colors as mpl_colors
 from diplomat.utils.colormaps import to_colormap
+from sleap.prefs import prefs
 import cv2
 
 def cv2_fourcc_string(val) -> int:
@@ -10,7 +11,7 @@ def cv2_fourcc_string(val) -> int:
 
 VISUAL_SETTINGS: ConfigSpec = {
     "pcutoff": (0.1, tc.RangedFloat(0, 1), "The probability to cutoff results below."),
-    "dotsize": (4, int, "The size of the dots."),
+    "dotsize": (int(prefs["marker size"]), int, "The size of the dots."),
     "alphavalue": (0.7, tc.RangedFloat(0, 1), "The alpha value of the dots."),
     "colormap": (None, to_colormap, "The colormap to use for tracked points in the video. Can be a matplotlib colormap or a list of matplotlib colors."),
     "shape_list": (None, tc.Optional(tc.List(str)), "A list of shape names, shapes to use for drawing each individual's dots."),
