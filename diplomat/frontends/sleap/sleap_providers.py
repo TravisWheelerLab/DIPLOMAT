Weighting in frame fixing/scoring added to account for low-confidence bodies. Normalization bug fixed in SLEAP frontend, clustering max-conv for peak finding fixed to produce 1 cell per peek.

diplomat/frontends/sleap/sleap_providers.py
@@ -60,10 +60,8 @@ class SleapModelExtractor(ABC):
         pass
 
 
-def _normalize_conf_map(conf_map: tf.Tensor) -> tf.Tensor:
-    conf_map = tf.maximum(conf_map, 0)
-    max_val = tf.reduce_max(conf_map, axis=(1, 2), keepdims=True)
-    return conf_map / max_val
+def _fix_conf_map(conf_map: tf.Tensor) -> tf.Tensor:
+    return tf.clip_by_value(conf_map, 0, 1)
 
 
 class BottomUpModelExtractor(SleapModelExtractor):
@@ -85,7 +83,7 @@ class BottomUpModelExtractor(SleapModelExtractor):
             raise ValueError("Scaling is not consistent!")
 
         return (
-            _normalize_conf_map(conf_map),
+            _fix_conf_map(conf_map),
             offsets if(offsets is None) else offsets,
             (1 / first_scale_val) * (1 / inf_layer.input_scale) * inf_layer.cm_output_stride
         )
@@ -165,7 +163,7 @@ class TopDownModelExtractor(SleapModelExtractor):
         conf_map, offset_map = _extract_model_outputs(inf_layer, imgs)
 
         return (
-            _normalize_conf_map(self._merge_tiles(conf_map, batch_size, (tiles_wide, tiles_high), (orig_w, orig_h), inf_layer.output_stride)),
+            _fix_conf_map(self._merge_tiles(conf_map, batch_size, (tiles_wide, tiles_high), (orig_w, orig_h), inf_layer.output_stride)),
             self._merge_tiles(offset_map, batch_size, (tiles_wide, tiles_high), (orig_w, orig_h), inf_layer.output_stride),
             (1 / inf_layer.input_scale) * inf_layer.output_stride
         )
@@ -192,7 +190,7 @@ class SingleInstanceModelExtractor(SleapModelExtractor):
             raise ValueError("Scaling is not consistent!")
 
         return (
-            _normalize_conf_map(conf_map),
+            _fix_conf_map(conf_map),
             offset_map,
             (1 / first_scale_val) * (1 / inf_layer.input_scale) * inf_layer.output_stride
         )
@@ -270,6 +268,8 @@ class PredictorExtractor:
         if(pred.inference_model is None):
             pred._initialize_inference_model()
 
+        print("NOPE")
+
         for ex in pred.pipeline.make_dataset():
             probs, offsets, downscale = self._model_extractor.extract(ex)
 
@@ -280,6 +280,7 @@ class PredictorExtractor:
             h, w = data.video.shape[1:3]
             trim_h, trim_w = int(np.ceil(h / downscale)), int(np.ceil(w / downscale))
             probs = probs[:, :trim_h, :trim_w]
+
             offsets = offsets[:, :trim_h, :trim_w]
 
             yield TrackingData(

diplomat/predictors/fpe/frame_passes/cluster_frames.py
@@ -226,11 +226,14 @@ class ClusterFrames(FramePass):
 
         lookup_table = _NumpyDict(to_keys(x, y), prob, 0)
 
+        # We perform a 3x3 max-convolution to find peaks.
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if(i == 0 and j == 0):
                     continue
-                keep_arr = keep_arr & (lookup_table[to_keys(x + j, y + i)] <= prob)
+                neighbor = lookup_table[to_keys(x + j, y + i)]
+                below_to_right = (i >= 0) & (j >= 0)
+                keep_arr = keep_arr & (neighbor <= prob if(below_to_right) else neighbor < prob)
 
         top_indexes = np.flatnonzero(keep_arr)
 

diplomat/predictors/fpe/frame_passes/create_skeleton.py
@@ -131,7 +131,8 @@ class CreateSkeleton(FramePass):
 
                         d = min(((x - x2) ** 2 + (y - y2) ** 2) ** 0.5, d)
 
-                    hist.add(d)
+                    if(np.isfinite(d)):
+                        hist.add(d)
 
                 for bp1 in range(s2, s2 + total_bp):
                     (p, x, y) = self._prior_max_locations[bp1]
@@ -149,7 +150,8 @@ class CreateSkeleton(FramePass):
 
                         d = min(((x - x2) ** 2 + (y - y2) ** 2) ** 0.5, d)
 
-                    hist.add(d)
+                    if(np.isfinite(d)):
+                        hist.add(d)
 
             self._current_frame = frame_index
             self._prior_max_locations = [val for val in self._max_locations]

diplomat/predictors/fpe/frame_passes/fix_frame.py
@@ -11,8 +11,9 @@ import diplomat.processing.type_casters as tc
 
 class FixFrame(FramePass):
     """
-    Scores frames by peak separation, and then selects a single frame to remain clustered (with peaks separated). The rest of the frames are restored,
-    and :py:plugin:`~diplomat.predictors.frame_passes.MITViterbi` uses the fixed frame as it's ground truth frame.
+    Scores frames by peak separation, and then selects a single frame to remain clustered (with peaks separated). The
+    rest of the frames are restored, and :py:plugin:`~diplomat.predictors.frame_passes.MITViterbi` uses the fixed
+    frame as it's ground truth frame.
     """
     SCORES_PER_CHUNK = 20
 
@@ -46,17 +47,18 @@ class FixFrame(FramePass):
         cls,
         frame: ForwardBackwardFrame,
         down_scaling: float
-    ) -> Tuple[Optional[float], Optional[float]]:
+    ) -> Tuple[Optional[float], Optional[float], Optional[float]]:
         y, x, prob, x_off, y_off = frame.src_data.unpack()
 
         if(prob is None):
-            return (None, None)
+            return (None, None, None)
 
         max_idx = np.argmax(prob)
 
         return (
             x[max_idx] + 0.5 + (x_off[max_idx] / down_scaling),
-            y[max_idx] + 0.5 + (y_off[max_idx] / down_scaling)
+            y[max_idx] + 0.5 + (y_off[max_idx] / down_scaling),
+            prob[max_idx]
         )
 
     @classmethod
@@ -135,13 +137,14 @@ class FixFrame(FramePass):
                     if(m1[0] is None or m2[0] is None):
                         group_dist_scores[g_i] = -np.inf
                     else:
-                        group_dist_scores[g_i] = min(cls.dist(m1, m2), group_dist_scores[g_i])
+                        group_dist_scores[g_i] = min(cls.dist(m1, m2) * m1[2] * m2[2], group_dist_scores[g_i])
 
         best = (0, 0)
         best_i = 0
 
         for i, new in enumerate(zip(degrees, group_dist_scores)):
-            if(new > best):
+            # We don't want to allow a high degree node to be used if it's distance is 0 for some of the nodes...
+            if(new[1] > 0 and new > best):
                 best = new
                 best_i = i
 
@@ -352,6 +355,9 @@ class FixFrame(FramePass):
         for bp_group_off in range(num_bp):
 
             min_dist = np.inf
+            total_conf = 0
+            count = 0
+
             # For body part groupings...
             for i in range(num_outputs - 1):
                 f1_loc = cls.get_max_location(
@@ -373,15 +379,19 @@ class FixFrame(FramePass):
                         continue
 
                     min_dist = min(cls.dist(f1_loc, f2_loc), min_dist)
+                    total_conf += f1_loc[2] * f2_loc[2]
+                    count += 1
 
             if(np.isinf(min_dist)):
                 min_dist = 0
-            if(min_dist == 0):
+            if(min_dist == 0 or count == 0):
                 # BAD! We found a frame that failed to cluster properly...
                 score = -np.inf
 
-            score += min_dist
-            score2 += min_dist
+            # Minimum distance, weighted by average skeleton-pair confidence...
+            if(count > 0):
+                score += min_dist * (total_conf / count)
+                score2 += min_dist * (total_conf / count)
 
         # If skeleton is implemented...
         if (skeleton is not None):