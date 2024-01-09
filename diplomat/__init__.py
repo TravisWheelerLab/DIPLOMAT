b52db749afc8ad32e50245431e3f998c4e0e5530
isaacrobinson2000 <awesomeisaac2000@gmail.com>
2024-01-09 17:00:14 -0500

    Version bump...

--- diplomat/__init__.py
+++ diplomat/__init__.py
@@ -2,7 +2,7 @@
 A tool providing multi-animal tracking capabilities on top of other Deep learning based tracking software.
 """
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 # Can be used by functions to determine if diplomat was invoked through it's CLI interface.
 CLI_RUN = False
 
