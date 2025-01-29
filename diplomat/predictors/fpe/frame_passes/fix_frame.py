35f3c9df37938ee12635f69312a9a7c79ba20ed8
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2025-01-29 15:11:27 -0700

    remove frame score normalization

--- diplomat/predictors/fpe/frame_passes/fix_frame.py
+++ diplomat/predictors/fpe/frame_passes/fix_frame.py
@@ -561,27 +561,10 @@ class FixFrame(FramePass):
         return (scores[:, 0], scores[:, 1])
 
     @classmethod
-    def normalize_score(
-        cls,
-        fb_data: ForwardBackwardData,
-        frame_score: float
-    ) -> float:
-        num_outputs = fb_data.metadata.num_outputs
-        # approximate the maximal minimum distance between num_outputs points spread in the unit square
-        maxmin_dist = approximate_maxmin_distance(num_outputs)
-        # scale the distance wrt the video resolution. 
-        # this is definitely not perfect for rectangular resolutions but it gets close enough.
-        scaled_maxmin_dist = maxmin_dist * np.sqrt(fb_data.metadata.width * fb_data.metadata.height)
-        # normalize the frame score
-        num_bp = len(fb_data.metadata.bodyparts)
-        return frame_score / (scaled_maxmin_dist * num_bp)
-
-    @classmethod
     def restore_all_except_fix_frame(
         cls,
         fb_data: ForwardBackwardData,
         frame_idx: int,
-        frame_score: float,
         fix_frame_data: List[ForwardBackwardFrame],
         prog_bar: ProgressBar,
         reset_bar: bool = False,
@@ -589,8 +572,6 @@ class FixFrame(FramePass):
     ) -> ForwardBackwardData:
         # For passes to use....
         fb_data.metadata.fixed_frame_index = int(frame_idx)
-        fb_data.metadata.fixed_frame_score = frame_score
-        fb_data.metadata.normalized_fixed_frame_score = cls.normalize_score(fb_data,frame_score)
         fb_data.metadata.is_pre_initialized = is_pre_initialized
         
         if(reset_bar and prog_bar is not None):
@@ -637,7 +618,6 @@ class FixFrame(FramePass):
 
         if(self.config.DEBUG):
             print(f"Max Scoring Frame: {self._max_frame_idx}")
-            print(f"Max Score: {self._max_frame_score}")
             
         self._fixed_frame = self.create_fix_frame(
             fb_data,
@@ -651,7 +631,6 @@ class FixFrame(FramePass):
             return self.restore_all_except_fix_frame(
                 fb_data,
                 self._max_frame_idx,
-                self._max_frame_score,
                 self._fixed_frame,
                 prog_bar,
                 False
